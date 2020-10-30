0949acb7854839bf4d185c2379fb68542806feba
Ethan <4lrce4@gmail.com>
2020-10-30 12:57:45 -0600

    use 'latin' encoding

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
 import pandas as pd
+import random
+
+file = pd.read_csv("catalog.csv", encoding='latin')
+
+# Print random author name
+print(file["AUTHOR"][random.randint(0, len(file["AUTHOR"]))])
