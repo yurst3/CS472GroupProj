eedf087d188a1cbb53b46028e3a7ba19b8459cf9
Ethan <4lrce4@gmail.com>
2020-11-02 14:48:33 -0700

    added option to skip files if they have already been downloaded to download_catalogue.py

--- download_catalogue.py
+++ download_catalogue.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from tqdm import tqdm
 from PIL import Image
+import os
 import requests
 
 # TODO: Exclude stuff that isn't paintings?
@@ -8,6 +9,9 @@ catalogue = pd.read_csv("catalog.csv", encoding='latin')
 
 # Fraction of paintings to use from the catalogue
 catalogue_fraction = 1/100
+
+# Skips over files if they are already downloaded
+skip_downloaded = True
 
 # Resize each downloaded image to these dimensions
 resize_dimensions = (128,128)
@@ -21,6 +25,16 @@ with tqdm(total=int(len(catalogue['URL'])*catalogue_fraction)) as pbar:
         url = catalogue['URL'][i].replace("/html", "/art").replace(".html", ".jpg")
         author = catalogue['AUTHOR'][i]
         title = catalogue['TITLE'][i]
+
+        # Create file path using the author and title of the artwork
+        # Remove extraneous " from the file path
+        jpg_file_path = f"images/{author} \'{title}\'.jpg".replace('\"', '')
+
+        # Skip over this file if it has already been downloaded
+        if skip_downloaded and os.path.isfile(jpg_file_path):
+            pbar.update(1)
+            continue
+
         pbar.set_description(f"Downloading {url}")
 
         # Send request to url
@@ -32,9 +46,6 @@ with tqdm(total=int(len(catalogue['URL'])*catalogue_fraction)) as pbar:
             pbar.update(1)
             continue
 
-        # Create file path using the author and title of the artwork
-        # Remove extraneous " from the file path
-        jpg_file_path = f"images/{author} \'{title}\'.jpg".replace('\"', '')
         with open(jpg_file_path, "wb") as image_jpg:
             image_jpg.write(response.content)
             im = Image.open(jpg_file_path)
